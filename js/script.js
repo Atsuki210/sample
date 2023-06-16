1deffd638640f99d0f113a7ca1cd9e5d62d9a5b6
First-name Family-name <115214769+Atsuki210@users.noreply.github.com>
2023-06-16 15:58:44 +0900

    aaa

--- js/script.js
+++ js/script.js
@@ -12,9 +12,10 @@ window.addEventListener('load', function () {
     });
 });
 
-$(function () {
-    $(window).scroll (function () {
+$(function() {
+    $(window).scroll(function() {
         $("nav.floating").stop().animate(
-            {"top": $(window).scrollTop() + 100},500);
+            {"top": $(window).scrollTop() + 100},
+        500);
     });
 });
